b855e592ab04d7735c23eebcdc5144a143a0e5ac
Nasafato <alanjgou@gmail.com>
2014-12-15 18:14:25 -0500

    Lesson 1 done

---
+++ js/resumeBuilder.js
@@ -0,0 +1,63 @@
+var bio = {
+   "name" : "Alan Gou",
+   "role" : "Programmer",
+   "welcomeMessage" : "Welcome to my site!",
+   "contacts" : {
+      "mobile": "908-251-0740",
+      "email": "alanjgou@gmail.com",
+      "github": "Nasafato",
+      "twitter": "@Nasafato",
+      "location": "New York Area"
+   },
+   "skills": [
+      "essay writing","SAT prep", "numpy and scipy", "being awesome"
+   ],
+   "bioPic" : "images/me.jpg"
+
+};
+
+
+var education = {
+   "schools": [
+      {
+         "name": "Rutgers University",
+         "degree": "B.S.",
+         "majors": ["compsci","math","econ"],
+         "graduationDate": "May 2017",
+         "url": "rutgers.edu",
+         "location": "New Brunswick, NJ"
+      }  
+   ],
+   "onlineCourses": [
+      {
+         "title" : "Javascript Basics",
+         "school" : "Udacity",
+         "dates" : "10/2014 = 1/2014",
+         "url" : "udacity.com"
+
+      }
+   ]
+};
+
+var projects = {
+   "projects": [
+      {
+         "title": "N/A",
+         "dates": "N/A",
+         "descriptions": "N/A",
+         "images": []
+      }
+   ]
+};
+
+var work = {
+   "jobs": [
+      {
+         "employer": "Rutgers University",
+         "title": "student",
+         "location": "New Brunswick",
+         "dates": "9/2013 - 5/2017",
+         "description": "Studied alot"
+      }
+   ]
+};
